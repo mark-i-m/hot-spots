ea1c982ac3ed7cb380664ea475eeab4b77c20369
Mark Mansi <markm@cs.wisc.edu>
2018-11-13 15:14:52 -0600

    add a simple test

--- tests/test1.cc
+++ tests/test1.cc
@@ -8,6 +8,8 @@
 #include "btree-bytereorder.h"
 
 #include <cassert>
+#include <iostream>
+#include <string.h>
 
 // Btree implementations to test
 enum class BTreeType {
@@ -17,31 +19,61 @@ enum class BTreeType {
 };
 
 // Test prototypes
-void test1(common::BTreeBase<uint64_t, uint64_t> *btree);
+void test_simple_insert_read(common::BTreeBase<uint64_t, uint64_t> *btree);
 
-int main() {
+void usage_and_exit() {
+    std::cout << "Usage: ./test <TREE TYPE>\n"
+        << "<TREE TYPE> := olc|hybrid|br"
+        << std::endl;
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    // Make sure at least one cmd line arg
+    if (argc < 2) {
+        usage_and_exit();
+    }
+
     // TODO: Based on command line args, choose which tree to construct.
-    BTreeType type = BTreeType::BTreeOLC;
+    BTreeType type;
+    if (strncmp("olc", argv[1], 4) == 0) {
+        type = BTreeType::BTreeOLC;
+    } else if (strncmp("hybrid", argv[1], 7) == 0) {
+        type = BTreeType::BTreeHybrid;
+    } else if (strncmp("br", argv[1], 3) == 0) {
+        type = BTreeType::BTreeByteReorder;
+    } else {
+        usage_and_exit();
+    }
 
     // Construct the btree implementation we want to test.
     common::BTreeBase<uint64_t, uint64_t> *btree;
     switch (type) {
         case BTreeType::BTreeOLC:
+            std::cout << "Testing OLC" << std::endl;
             btree = new btreeolc::BTree<uint64_t, uint64_t>();
             break;
         case BTreeType::BTreeHybrid:
+            std::cout << "Testing Hybrid" << std::endl;
             btree = new btree_hybrid::BTree<uint64_t, uint64_t>();
             break;
         case BTreeType::BTreeByteReorder:
+            std::cout << "Testing Byte Reordering" << std::endl;
             btree = new btree_bytereorder::BTree<uint64_t, uint64_t>();
             break;
     }
 
     // Run tests
-    test1(btree);
+    test_simple_insert_read(btree);
+
+    // Done!
+    std::cout << "SUCCESS :)" << std::endl;
 }
 
-void test1(common::BTreeBase<uint64_t, uint64_t> *) {
-    assert(false);
-    // TODO
+void test_simple_insert_read(common::BTreeBase<uint64_t, uint64_t> *btree) {
+    btree->insert(0, 0);
+
+    uint64_t v;
+    assert(btree->lookup(0, v));
+    assert(v == 0);
 }
