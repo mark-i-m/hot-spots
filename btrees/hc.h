16436ce748f1c63834feed28faaeb27f9bc5bb6a
Mark Mansi <markm@cs.wisc.edu>
2018-11-15 22:53:11 -0600

    make HC thread-safe, needs finer-grained locking

--- btrees/hc.h
+++ btrees/hc.h
@@ -1,8 +1,11 @@
 #ifndef _BTREE_HC_H_
 #define _BTREE_HC_H_
 
+// TODO: finer-grained locking
+
 #include "util.h"
 #include <unordered_map>
+#include <pthread.h>
 
 namespace btree_hybrid {
 
@@ -10,6 +13,12 @@ template <typename K, typename V>
 struct HC {
     // Creating a `Map` type from Keys to Values
     typedef std::unordered_map<K, V> Map;
+    // Lock
+    mutable pthread_rwlock_t lock;
+
+    HC();
+
+    ~HC();
 
     // Insert a key and value in a page that is already hot
     void insert(K k, V v);
@@ -27,17 +36,31 @@ private:
     util::RangeMap<K, Map> hot_cache;
 };
 
+template <typename K, typename V>
+HC<K, V>::HC() {
+    int ret = pthread_rwlock_init(&lock, NULL);
+    assert(ret == 0);
+}
+
+template <typename K, typename V>
+HC<K, V>::~HC() {
+    int ret = pthread_rwlock_destroy(&lock);
+    assert(ret == 0);
+}
 
 template <typename K, typename V>
 void HC<K, V>::insert(K k, V v) {
+    pthread_rwlock_wrlock(&lock);
     auto maybe = hot_cache.find(k);
     assert(maybe);
     (*maybe)->insert(k, v);
+    pthread_rwlock_unlock(&lock);
 }
 
 
 template <typename K, typename V>
 void HC<K, V>::insert(K kl, K kh, K k, V v) {
+    pthread_rwlock_wrlock(&lock);
     auto maybe = hot_cache.find(k);
 
     // If the range is not in the RangeMap, insert it along with the given Key and Value
@@ -50,6 +73,7 @@ void HC<K, V>::insert(K kl, K kh, K k, V v) {
     else {
         (*maybe)->insert({k, v});
     }
+    pthread_rwlock_unlock(&lock);
 }
 
 template <typename K, typename V>
@@ -60,14 +84,21 @@ typename HC<K, V>::Map HC<K, V>::remove(const K& kl, const K& kh) {
 
 template <typename K, typename V>
 util::maybe::Maybe<V *> HC<K, V>::find(const K& k) {
+    pthread_rwlock_rdlock(&lock);
     auto maybe = hot_cache.find(k);
     if(!maybe) {
+        pthread_rwlock_unlock(&lock);
         return util::maybe::Maybe<V *>();
     } else {
         auto it = (*maybe)->find(k);
         if(it != (*maybe)->end()) {
-            return util::maybe::Maybe<V *>(&it->second);
+            // TODO: Pointer maybe invalidated after we release the lock? Make
+            // this simpler by using an int instead
+            auto maybe = util::maybe::Maybe<V *>(&it->second);
+            pthread_rwlock_unlock(&lock);
+            return maybe;
         } else {
+            pthread_rwlock_unlock(&lock);
             return util::maybe::Maybe<V *>();
         }
     }
